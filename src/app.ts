9b32e41965a237350b812bab5500a58d0f7f5487
anowarzz <anowarhosen444@gmail.com>
2025-09-06 09:37:08 +0600

    chore: project initialized and connect github

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import cors from "cors";
+import express, {
+  type Application,
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+
+const app: Application = express();
+
+// Parser
+app.use(express.json());
+app.use(cors());
+
+// Testing API HomeRoute
+const test = async (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome To The App Server",
+    note: "Great things takes time",
+  });
+};
+
+app.get("/", test);
+
+// route error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+export default app;
